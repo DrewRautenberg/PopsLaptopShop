db95c77005f516f36124ba01475939a39d3f3e3c
Drew Rautenberg <drew@drewrautenberg.com>
2017-09-20 14:20:57 -0400

    Fixed mistake of code not being inside main method

--- src/PopsLaptopShop.java
+++ src/PopsLaptopShop.java
@@ -3,27 +3,34 @@
     * Description:
     */
 public class PopsLaptopShop {
-    //Variable Declaration
+        public static void main(String[] args) {
+            //Variable Declaration
+            //laptop types
+            double basic = 450.90;
+            double premium = 850.50;
+            double delux = 1350.95;
 
-    //laptop types
-    double basic = 450.90;
-    double premium = 850.50;
-    double delux = 1350.95;
+            //commission amounts
+            int basicComm = 50;
+            int premiumComm = 100;
+            int deluxComm = 150;
+            final int SUPER_BONUS = 1000;
 
-    //commission amounts
-    int basicComm = 50;
-    int premiumComm = 100;
-    int deluxComm = 150;
-    final int SUPER_BONUS = 1000;
+            //user information
+            String name;
+            int baseSalary;
+            char tier;
+            int numBasic = 0;
+            int numPremium = 0;
+            int numDelux = 0;
 
-    //user information
-    String name;
-    int baseSalary;
-    char tier;
-    int numBasic;
-    int numPremium;
-    int numDelux;
+            //Calculations
+            double salesTotal = (numBasic * basic) + (numPremium * premium) + (numDelux * delux);
 
+            //Calculate Bonus Amount
+
+
+        }
 
 
 

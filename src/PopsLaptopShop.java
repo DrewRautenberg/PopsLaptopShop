28a3e4de5c220eab5c7bad12537861fb05a4d668
Drew Rautenberg <drew@drewrautenberg.com>
2017-09-20 14:08:03 -0400

    Variable Declaration

--- src/PopsLaptopShop.java
+++ src/PopsLaptopShop.java
@@ -3,4 +3,28 @@
     * Description:
     */
 public class PopsLaptopShop {
+    //Variable Declaration
+
+    //laptop types
+    double basic = 450.90;
+    double premium = 850.50;
+    double delux = 1350.95;
+
+    //commission amounts
+    int basicComm = 50;
+    int premiumComm = 100;
+    int deluxComm = 150;
+    final int SUPER_BONUS = 1000;
+
+    //user information
+    String name;
+    int baseSalary;
+    char tier;
+    int numBasic;
+    int numPremium;
+    int numDelux;
+
+
+
+
 }

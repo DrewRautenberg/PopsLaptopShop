cd18115f60c34b5830e50390802d70f4526c8907
Drew Rautenberg <drew@drewrautenberg.com>
2017-09-27 08:32:17 -0400

    added do while loop to enter another employee

--- src/PopsLaptopShop.java
+++ src/PopsLaptopShop.java
@@ -40,109 +40,115 @@ public class PopsLaptopShop {
             double totCommBonus;
             double totMonthPay;
             String bonusMessage = "";
+            String repeatInput;
+            boolean repeat = false;
 
-            //user input
-            name = JOptionPane.showInputDialog("Name");
             do {
-                baseSalary = Double.parseDouble(JOptionPane.showInputDialog("Base salary"));
-            }while (baseSalary > MAX_VALID_SALARY);
-            startingTier =JOptionPane.showInputDialog("Tier (T, M, or S)");
-            do {
-                numBasic = Integer.parseInt(JOptionPane.showInputDialog("Number of basic laptops sold"));
-            }while (numBasic > MAX_VALID_UNITS);
-            do {
-                numPremium = Integer.parseInt(JOptionPane.showInputDialog("Number of premium laptops sold"));
-            }while (numPremium > MAX_VALID_UNITS);
-            do {
-                numDeluxe = Integer.parseInt(JOptionPane.showInputDialog("Number of deluxe laptops sold"));
-            }while (numDeluxe > MAX_VALID_UNITS);
 
-            //Convert starting tier from single letter string to word
-            switch (startingTier){
-                case "S":
-                    startingTierWord = "Starting";
-                    break;
-                case "M":
-                    startingTierWord = "Middle";
-                    break;
-                case "T":
-                    startingTierWord = "Top";
-                    break;
-                default:
-                    startingTierWord = "Starting";
-                    break;
-            }
+                //user input
+                name = JOptionPane.showInputDialog("Name");
+                do {
+                    baseSalary = Double.parseDouble(JOptionPane.showInputDialog("Base salary"));
+                } while (baseSalary > MAX_VALID_SALARY);
+                startingTier = JOptionPane.showInputDialog("Tier (T, M, or S)");
+                do {
+                    numBasic = Integer.parseInt(JOptionPane.showInputDialog("Number of basic laptops sold"));
+                } while (numBasic > MAX_VALID_UNITS);
+                do {
+                    numPremium = Integer.parseInt(JOptionPane.showInputDialog("Number of premium laptops sold"));
+                } while (numPremium > MAX_VALID_UNITS);
+                do {
+                    numDeluxe = Integer.parseInt(JOptionPane.showInputDialog("Number of deluxe laptops sold"));
+                } while (numDeluxe > MAX_VALID_UNITS);
 
-            //Calculate sales total
-            salesTotal = (numBasic * basic) + (numPremium * premium) + (numDeluxe * deluxe);
-
-            //Calculate commission amount
-            commTotal = (numBasic * basicComm) + (numPremium * premiumComm) + (numDeluxe * deluxeComm);
-
-            //Calculate Bonus Amount
-            if ((salesTotal >= 0) && (salesTotal <2500)){
-                bonusAmt = 0;
-            }
-            else if ((salesTotal >= 2500) && (salesTotal <5500)){
-                bonusAmt = salesTotal *0.01;
-            }
-            else if ((salesTotal >= 5500) && (salesTotal <10500)){
-                bonusAmt = 75 + (0.02 * (salesTotal - 5500));
-            }
-            else if ((salesTotal >= 10500) && (salesTotal <13500)){
-                bonusAmt = 125 + (0.03 * (salesTotal - 10500));
-            }
-            else {
-                bonusAmt = 375;
-            }
-
-            //Calculate 75% of base salary
-            baseSalaryLimit = baseSalary *.75;
-
-            //Determine ending tier
-            if (commTotal >= baseSalaryLimit){
-                switch (startingTier){
+                //Convert starting tier from single letter string to word
+                switch (startingTier) {
                     case "S":
-                        finalTier = "Middle";
-                        bonusMessage = "Congratulations, " + name + ", you got promoted to the Middle tier.";
+                        startingTierWord = "Starting";
                         break;
                     case "M":
-                        finalTier = "Top";
-                        bonusMessage = "Congratulations, " + name + ", you got promoted to the Top tier.";
+                        startingTierWord = "Middle";
                         break;
                     case "T":
-                        finalTier = "Top";
-                        superBonus = SUPER_BONUS;
-                        bonusMessage = "Congratulations, " + name + ", you got the Super Bonus because you were " +
-                                "already in the top tier.";
+                        startingTierWord = "Top";
                         break;
                     default:
-                        finalTier = startingTierWord;
+                        startingTierWord = "Starting";
                         break;
                 }
-            }
-            else{
-                finalTier = startingTierWord;
-                bonusMessage = "Sorry, " + name + ", you didn't get promoted this month.";
-            }
 
-            //Calculate total commission and bonus
-            totCommBonus = commTotal + bonusAmt + superBonus;
+                //Calculate sales total
+                salesTotal = (numBasic * basic) + (numPremium * premium) + (numDeluxe * deluxe);
 
-            //Calculate total pay for the month
-            totMonthPay = baseSalary + totCommBonus;
+                //Calculate commission amount
+                commTotal = (numBasic * basicComm) + (numPremium * premiumComm) + (numDeluxe * deluxeComm);
 
-            //Outputs
-            System.out.println("Salesperson: " + name);
-            System.out.println("Starting Tier: " + startingTierWord);
-            System.out.printf("Base Salary: $%,.2f\n", baseSalary);
-            System.out.printf("Commission: $%,.2f\n", commTotal);
-            System.out.printf("Bonus: $%,.2f\n", bonusAmt);
-            System.out.printf("Super Bonus: $%,.2f\n", superBonus);
-            System.out.printf("Total Commission and Bonus: $%,.2f\n", totCommBonus);
-            System.out.printf("Total Monthly Pay: $%,.2f\n", totMonthPay);
-            System.out.println("Final Tier: " + finalTier);
-            System.out.println(bonusMessage);
+                //Calculate Bonus Amount
+                if ((salesTotal >= 0) && (salesTotal < 2500)) {
+                    bonusAmt = 0;
+                } else if ((salesTotal >= 2500) && (salesTotal < 5500)) {
+                    bonusAmt = salesTotal * 0.01;
+                } else if ((salesTotal >= 5500) && (salesTotal < 10500)) {
+                    bonusAmt = 75 + (0.02 * (salesTotal - 5500));
+                } else if ((salesTotal >= 10500) && (salesTotal < 13500)) {
+                    bonusAmt = 125 + (0.03 * (salesTotal - 10500));
+                } else {
+                    bonusAmt = 375;
+                }
 
+                //Calculate 75% of base salary
+                baseSalaryLimit = baseSalary * .75;
+
+                //Determine ending tier
+                if (commTotal >= baseSalaryLimit) {
+                    switch (startingTier) {
+                        case "S":
+                            finalTier = "Middle";
+                            bonusMessage = "Congratulations, " + name + ", you got promoted to the Middle tier.";
+                            break;
+                        case "M":
+                            finalTier = "Top";
+                            bonusMessage = "Congratulations, " + name + ", you got promoted to the Top tier.";
+                            break;
+                        case "T":
+                            finalTier = "Top";
+                            superBonus = SUPER_BONUS;
+                            bonusMessage = "Congratulations, " + name + ", you got the Super Bonus because you were " +
+                                    "already in the top tier.";
+                            break;
+                        default:
+                            finalTier = startingTierWord;
+                            break;
+                    }
+                } else {
+                    finalTier = startingTierWord;
+                    bonusMessage = "Sorry, " + name + ", you didn't get promoted this month.";
+                }
+
+                //Calculate total commission and bonus
+                totCommBonus = commTotal + bonusAmt + superBonus;
+
+                //Calculate total pay for the month
+                totMonthPay = baseSalary + totCommBonus;
+
+                //Outputs
+                System.out.println("Salesperson: " + name);
+                System.out.println("Starting Tier: " + startingTierWord);
+                System.out.printf("Base Salary: $%,.2f\n", baseSalary);
+                System.out.printf("Commission: $%,.2f\n", commTotal);
+                System.out.printf("Bonus: $%,.2f\n", bonusAmt);
+                System.out.printf("Super Bonus: $%,.2f\n", superBonus);
+                System.out.printf("Total Commission and Bonus: $%,.2f\n", totCommBonus);
+                System.out.printf("Total Monthly Pay: $%,.2f\n", totMonthPay);
+                System.out.println("Final Tier: " + finalTier);
+                System.out.println(bonusMessage);
+
+                //prompt user if they would like to run again
+                repeatInput = JOptionPane.showInputDialog("Would you like to enter another sales person?");
+                if (repeatInput.equalsIgnoreCase("yes")){
+                    repeat = true;
+                }
+
+            }while (repeat);
         }
 }
